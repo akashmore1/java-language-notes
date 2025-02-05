e7b6c03a518f977c7302935a9095a31b6d14e605
Akash More <akashkmore1@gmail.com>
2025-02-05 19:28:54 +0530

    Print first number

---
+++ 0001-First_Steps/FirstNumber.java
@@ -0,0 +1,11 @@
+public class FirstNumber {
+
+    private static int getNumber(int num) {
+        return num;
+    }
+
+    public static void main(String[] args) {
+        int num = getNumber(4);
+        System.out.println(num);
+    }
+}
