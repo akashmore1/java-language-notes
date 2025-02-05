b0888ed6aaddf12a94334db0426da6ae016e723f
Akash More <akashkmore1@gmail.com>
2025-02-05 20:20:43 +0530

    Java smallest and largest integer

---
+++ 0001-First_Steps/code/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int smallestInt = Integer.MIN_VALUE;
+        int largestInt = Integer.MAX_VALUE;
+
+        System.out.println(smallestInt + " " + largestInt);
+    }
+}
