57837db522f040be811bfbbbee08f4cdc47e5ae2
Akash More <akashkmore1@gmail.com>
2025-02-09 21:32:58 +0530

    Solve carpet cost problem

---
+++ 04-Classes_Objects_Constructors/code/CarpetCostCalculator/Calculator.java
@@ -0,0 +1,15 @@
+// This code works on intelij
+// Commenting because it's not working on vscode because of some extension
+public class Calculator {
+    // Floor floor;
+    // Carpet carpet;
+
+    // public Calculator(Floor floor, Carpet carpet) {
+    // this.floor = floor;
+    // this.carpet = carpet;
+    // }
+
+    // public double getTotalCost() {
+    // return floor.getArea() * carpet.getCost();
+    // }
+}
